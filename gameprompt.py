dc584a3a466fe97d72d015762b4d922ec07ee4d9
Frye <hlfrye@gmail.com>
2022-04-15 00:24:57 -0500

    Working on initial commands

--- gameprompt.py
+++ gameprompt.py
@@ -12,4 +12,17 @@ class GamePrompt(Cmd):
         print("Farewell!")
         return True
 
+    def do_look(self, inp):
+        print("You look, but don't find anything")
+    
+    def do_search(self, inp):
+        print("After searching the room, you come up empty")
 
+    def do_walk(self, inp):
+        try:
+          self.player.move(inp)
+          new_pos = self.player.pos
+          room = self.board.get_room(new_pos)
+          print(room.description())
+        except: 
+          print("A mysterious force blocks you")
